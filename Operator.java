3c1f91efc3e4498864ad4b6dbefc1d8e3dea0cbe
qihuan <422548642@qq.com>
2022-10-20 10:12:53 +0800

    no.001

---
+++ Operator.java
@@ -0,0 +1,23 @@
+import vip.epss.util.DButil;
+
+import java.sql.*;
+
+public class Operator {
+    public static void main(String[] args) {
+
+    }
+}
+class Operatoradmin{
+    private Connection conn = null;
+    private Statement stat = null;
+    private ResultSet rs = null;
+    private PreparedStatement ps=null;
+
+    public  Operatoradmin() throws SQLException{
+        conn = DButil.getConn();
+        stat = conn.createStatement();
+    }
+    public void closeRes(){
+        DButil.close(null,stat,conn);
+    }
+}
